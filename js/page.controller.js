a9a099e9a7ab9b1c75974cceb09a6e923df4015b
Massi95 <massimiliano.fiore1995@gmail.com>
2017-06-08 16:08:48 +0200

    Ricerca Funzionante

--- js/page.controller.js
+++ js/page.controller.js
@@ -1,3 +1,30 @@
-app.controller('pageController', function($scope, $http, $routeParams) {
-    $scope.title = $routeParams.title
+
+
+
+app.factory('wikiService', function($http) {
+
+    var wikiService = {
+        get: function(title) {
+            return $http.jsonp('http://en.wikipedia.org/w/api.php?titles=' + title + '&rawcontinue=true&action=query&format=json&prop=extracts&callback=JSON_CALLBACK');
+        }
+    };
+    return wikiService;
+});
+
+
+app.controller('pageController', function($scope, $http, $routeParams, wikiService, $sce) {
+    $scope.$sce = $sce;
+	var wiki = this;
+      
+   	wiki.getInfo = function(){
+        wikiService.get($routeParams.title).then(function(data) {
+            wiki.wikiData = data.data;
+         	var id = Object.keys(wiki.wikiData.query.pages)[0];
+       	 	wiki.text = wiki.wikiData.query.pages[id].extract;
+            wiki.title = wiki.wikiData.query.pages[id].title;
+        });
+    }	// getWikiInfo
+
+    wiki.getInfo();
+    
 });
